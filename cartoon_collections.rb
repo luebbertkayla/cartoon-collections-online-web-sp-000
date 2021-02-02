20dd28e935c4c11c5fb84173756150a6061b2539
Kayla Luebbert <luebbertkayla@gmail.com>
2021-02-02 18:03:05 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,6 @@ end
 
 def summon_captain_planet(array)
   first_letter_cap = array.collect {|planeteer_call| planeteer_call.array[i].character(0).upcase}
-  
 end
 
 def long_planeteer_calls# code an argument here
