f283fd607c33efd4d1be7e66bfe759c0ea276ff6
Kayla Luebbert <luebbertkayla@gmail.com>
2021-02-02 18:07:31 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,7 @@ end
 
 def summon_captain_planet(array)
  array.collect do |planeteer_call|
-   planeteer_call.capitalize
+   planeteer_call.capitalize + "!"
 end
 
 def long_planeteer_calls# code an argument here
