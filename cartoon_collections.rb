d9283dd3c52f89e5f55792980cfc082bacd6288f
Kayla Luebbert <luebbertkayla@gmail.com>
2021-02-02 18:37:48 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -25,7 +25,6 @@ def long_planeteer_calls(calls)
   return false
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
